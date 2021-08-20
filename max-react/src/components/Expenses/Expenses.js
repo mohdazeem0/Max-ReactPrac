89d147956fa7e15f6d22817a850e622e7f5203f5
mohdazeem0 <mohdazm007@gmail.com>
2021-08-20 22:16:11 +0530

    update

--- max-react/src/components/Expenses/Expenses.js
+++ max-react/src/components/Expenses/Expenses.js
@@ -3,7 +3,7 @@ import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
 import Card from "../UI/Card";
 
-function Expenses(props) {
+const Expenses = (props) => {
   return (
     <Card className="expenses">
       <ExpenseItem
@@ -28,5 +28,5 @@ function Expenses(props) {
       ></ExpenseItem>
     </Card>
   );
-}
+};
 export default Expenses;
